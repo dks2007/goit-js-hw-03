2b7263d47fa9022a28f4b853e3b3acdcdf35acdc
Dima Karpinskyi <dimakjkarpinski@gmail.com>
2025-11-11 20:27:48 +0300

    hw3

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,13 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    const totalPrice = quantity * pricePerDroid;
-    if (totalPrice > customerCredits) {
-        return "Insufficient funds!"
-    }
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+function slugify(title) {
+    const slug = title
+        .toLowerCase() //!SECTION переводим в нижній регістр
+        .split(' ') //!SECTION  розбиваємо рядок на масив слів
+        .join('-'); //!SECTION  об'єднуємо масив слів у рядок, розділяючи їх дефісами
+    return slug;
 }
 
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
